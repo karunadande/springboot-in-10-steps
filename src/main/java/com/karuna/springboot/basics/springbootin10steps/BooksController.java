af3c61810fb551c9e1523e8d7fd07e6c9d81ccd3
karuna11 <dandekaruna047@gmail.com>
2024-03-28 00:06:21 +0530

    first commit

---
+++ src/main/java/com/karuna/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,18 @@
+package com.karuna.springboot.basics.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+
+	@GetMapping("/books")
+	public List<Book> getAllbooks(){
+		return Arrays.asList(
+				new Book(1,"Mastring spring 5.0","Karuna Dande"),
+				new Book(2,"Mastring Hibernate 5.0","Prashant Dhanorkar"));
+	}
+}
---
+++ src/main/java/com/karuna/springboot/basics/springbootin10steps/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.karuna.springboot.basics.springbootin10steps;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootIn10StepsApplication.class);
+	}
+
+}
